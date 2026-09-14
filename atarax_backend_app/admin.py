66bd4258b6d228857482cf3dcfa364c39e1c7618
Tomáš Gallischek <t.gall@seznam.cz>
2026-09-14 17:52:23 +0200

    aktualizace kódu (14.09.2026 17:52)

--- atarax_backend_app/admin.py
+++ atarax_backend_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Location, NPC
+from .models import Location, NPC, Player
 
 admin.site.register(Location)
 admin.site.register(NPC)
+admin.site.register(Player)
