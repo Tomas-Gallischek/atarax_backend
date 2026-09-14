27198cc28e69630e7c2abc9becbcd107018ae865
Tomáš Gallischek <t.gall@seznam.cz>
2026-09-14 08:41:32 +0200

    test

--- api_app/views.py
+++ api_app/views.py
@@ -2,14 +2,14 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 
 
 # INDEX
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@permission_classes([AllowAny])
 def index(request):
     print("Funkce index byla zavolána!")
     
