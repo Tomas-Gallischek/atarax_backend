499f974e7d1f96dd6886b61cd27ec031428be110
Tomáš Gallischek <t.gall@seznam.cz>
2026-09-14 07:45:35 +0200

    přepracování API

---
+++ api_app/views.py
@@ -0,0 +1,20 @@
+# NAČÍTÁNÍ KNIHOVEN
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+
+
+
+# INDEX
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def index(request):
+    print("Funkce index byla zavolána!")
+    
+    try:
+        return Response({"message": "Index OK"}, status=status.HTTP_200_OK)
+
+    except (TypeError, ValueError):
+        return Response({"error": "Někde se stala chyba"}, status=status.HTTP_400_BAD_REQUEST)
